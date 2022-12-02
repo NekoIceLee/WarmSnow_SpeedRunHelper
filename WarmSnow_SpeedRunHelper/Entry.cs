19862956413912c918909f16fe2152bcff9374b7
KrissLee <956140921@qq.com>
2022-12-02 19:34:47 +0800

    整理类文档

--- WarmSnow_SpeedRunHelper/Entry.cs
+++ WarmSnow_SpeedRunHelper/Entry.cs
@@ -25,50 +25,7 @@ namespace WarmSnow_SpeedRunHelper
         ConfigDefinition preset2Def = new ConfigDefinition("Preset", "Preset2");
         ConfigDefinition preset3Def = new ConfigDefinition("Preset", "Preset3");
         ConfigDefinition preset4Def = new ConfigDefinition("Preset", "Preset4");
-        public static Preset Preset1
-        {
-            get
-            {
-                return JsonUtility.FromJson<Preset>(preset1JsonString.Value);
-            }
-            set
-            {
-                preset1JsonString.Value = JsonUtility.ToJson(value);
-            }
-        }
-        public static Preset Preset2
-        {
-            get
-            {
-                return JsonUtility.FromJson<Preset>(preset2JsonString.Value);
-            }
-            set
-            {
-                preset2JsonString.Value = JsonUtility.ToJson(value);
-            }
-        }
-        public static Preset Preset3
-        {
-            get
-            {
-                return JsonUtility.FromJson<Preset>(preset3JsonString.Value);
-            }
-            set
-            {
-                preset3JsonString.Value = JsonUtility.ToJson(value);
-            }
-        }
-        public static Preset Preset4
-        {
-            get
-            {
-                return JsonUtility.FromJson<Preset>(preset4JsonString.Value);
-            }
-            set
-            {
-                preset4JsonString.Value = JsonUtility.ToJson(value);
-            }
-        }
+        
         void Start()
         {
             preset1JsonString = Config.Bind(preset1Def, "");
@@ -85,22 +42,5 @@ namespace WarmSnow_SpeedRunHelper
         }
     }
 
-    public class PresetControl
-    {
-        public static PresetControl Instance { get; set; } = new PresetControl();
-        ConfigEntry<string> Preset1;
-        PresetControl()
-        {
-            Preset1 = ConfigFile.CoreConfig.Bind("Preset", "Preset1", "", "Preset1");
-        }
-        
-    }
-    [Serializable]
-    public class Preset
-    {
-        public int SectSkill { get; set; }
-        public int FirstSkill { get; set; }
-        public PN Potion1 { get; set; }
-        public PN Potion2 { get; set; }
-    }
+    
 }
---
+++ WarmSnow_SpeedRunHelper/PresetContainer.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace WarmSnow_SpeedRunHelper
+{
+    public class PresetControl
+    {
+        public static PresetControl Instance { get; set; } = new PresetControl();
+        public static Preset Preset1
+        {
+            get
+            {
+                return JsonUtility.FromJson<Preset>(HelperMainModule.preset1JsonString.Value);
+            }
+            set
+            {
+                HelperMainModule.preset1JsonString.Value = JsonUtility.ToJson(value);
+            }
+        }
+        public static Preset Preset2
+        {
+            get
+            {
+                return JsonUtility.FromJson<Preset>(HelperMainModule.preset2JsonString.Value);
+            }
+            set
+            {
+                HelperMainModule.preset2JsonString.Value = JsonUtility.ToJson(value);
+            }
+        }
+        public static Preset Preset3
+        {
+            get
+            {
+                return JsonUtility.FromJson<Preset>(HelperMainModule.preset3JsonString.Value);
+            }
+            set
+            {
+                HelperMainModule.preset3JsonString.Value = JsonUtility.ToJson(value);
+            }
+        }
+        public static Preset Preset4
+        {
+            get
+            {
+                return JsonUtility.FromJson<Preset>(HelperMainModule.preset4JsonString.Value);
+            }
+            set
+            {
+                HelperMainModule.preset4JsonString.Value = JsonUtility.ToJson(value);
+            }
+        }
+        PresetControl()
+        {
+            
+        }
+
+    }
+    [Serializable]
+    public class Preset
+    {
+        public int SectSkill { get; set; }
+        public int FirstSkill { get; set; }
+        public PN Potion1 { get; set; }
+        public PN Potion2 { get; set; }
+    }
+}

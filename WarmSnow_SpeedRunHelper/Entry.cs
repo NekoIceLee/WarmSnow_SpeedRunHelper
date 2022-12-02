01cfaaf0938ffea5183a8dc6315262b8ca84b19c
KrissLee <956140921@qq.com>
2022-12-02 15:48:38 +0800

    添加项目文件。

---
+++ WarmSnow_SpeedRunHelper/Entry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Core;
+using HarmonyLib;
+using UnityEngine;
+
+namespace WarmSnow_SpeedRunHelper
+{
+    [BepInPlugin("com.NekoIce.SpeedRunHelper", "SpeedRunHelper", "0.0.1")]
+    public class HelperMainModule : BaseUnityPlugin
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+
+    }
+}

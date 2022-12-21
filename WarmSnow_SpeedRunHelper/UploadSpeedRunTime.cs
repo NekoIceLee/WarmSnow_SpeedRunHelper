33ae8d9ee35239e38cef866b981695507bb31881
KrissLee <956140921@qq.com>
2022-12-21 21:02:24 +0800

    初步添加对mysql上传成绩的支持

---
+++ WarmSnow_SpeedRunHelper/UploadSpeedRunTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WarmSnow_SpeedRunHelper
+{
+    internal class UploadSpeedRunTime
+    {
+        public static UploadSpeedRunTime Instance { get; } = new UploadSpeedRunTime();
+        public string PlayerName { get; set; }
+        readonly string SQLServerAddr = "gz-cdb-7erg1rwj.sql.tencentcdb.com";
+        readonly int SQLServerPort = 63972;
+        readonly string ClientUser = "";
+        readonly string ClientPasswd = "";
+        MySqlConnection _connection;
+        
+        UploadSpeedRunTime()
+        {
+            _connection = new MySqlConnection($"server={SQLServerAddr},{SQLServerPort};userid={ClientUser};password={ClientPasswd};database=application;");
+        }
+
+    }
+}
